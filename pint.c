f543e3f06b2c983aaa856bd9e7265707b785805e
Modupi <modupimaphoto@gmail.com>
2023-12-12 09:33:45 -0800

    1. pint

---
+++ pint.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * pint - Prints the value at the top of the stack
+ *
+ * @stack: Pointer to the stack
+ * @line_number: Line number of the instruction
+ *
+ * Return: Nothing.
+ */
+void pint(stack_t *stack, unsigned int line_number)
+{
+	if (stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", stack->n);
+}
